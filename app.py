3167fffaf104af47e4b55321e9a2451fdcbf937e
Sergio Aota <144942166+sergioaota@users.noreply.github.com>
2025-09-09 11:55:53 -0300

    aula 1

---
+++ app.py
@@ -0,0 +1,14 @@
+print("""
+╭━╮╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━┳━╮
+┃━╋━╮┃╰┳━┳┳╮╭━┳┳┳━┳┳┳━┫━┫━┫
+┣━┃╋╰┫╋┃╋┃╭╯┃┻╋┃┫╋┃╭┫┻╋━┣━┃
+╰━┻━━┻━┻━┻╯╱╰━┻┻┫╭┻╯╰━┻━┻━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯""")
+
+print('1. Cadastras Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
