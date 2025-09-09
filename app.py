69484edecc53dc488eba4146f85b8739ce5bf0a0
Sergio Aota <144942166+sergioaota@users.noreply.github.com>
2025-09-09 15:44:43 -0300

    aula 2
    aula 2

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+    print("""
 ╭━╮╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━┳━╮
 ┃━╋━╮┃╰┳━┳┳╮╭━┳┳┳━┳┳┳━┫━┫━┫
 ┣━┃╋╰┫╋┃╋┃╭╯┃┻╋┃┫╋┃╭┫┻╋━┣━┃
 ╰━┻━━┻━┻━┻╯╱╰━┻┻┫╭┻╯╰━┻━┻━╯
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯""")
 
-print('1. Cadastras Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastras Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def finalizar_app():
+    os.system("cls")
+    # os.system("clear") no Mac
+    print("Encerrando o Programa")
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+
+    if opcao_escolhida == 1:
+        print("Cadastrar Restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar Restaurante")
+    elif opcao_escolhida == 3:
+        print("Ativar Restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
